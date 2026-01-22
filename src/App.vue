104a9abcb356c0f9c3e34c7ee9c76686b67c3e29
e <1263538771@qq.com>
2026-01-22 18:02:27 +0800

    vue3

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h2>宝宝学英语</h2>
+    <p>欢迎来到宝宝学英语</p>
+    <p>在这里，宝宝可以学习英语</p>
+    <p>宝宝正在学习英语</p>
+    <p>宝宝已经学习了英语</p>
+    <p>宝宝喜欢学习英语</p>
+    <p>宝宝学英语很认真</p>
+    <p>宝宝学英语很开心</p>
+    <p>宝宝学英语很努力</p>
+    <p>宝宝学英语很聪明</p>
+    <p>宝宝学英语很进步</p>
+    <p>宝宝学英语很优秀</p>
+    <p>宝宝学英语很成功</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
